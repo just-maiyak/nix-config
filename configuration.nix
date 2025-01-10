cf0a25bf432958d0e1f9c944460e5a94602d2cda
Marc Yefimchuk <marc.yefimchuk@radiofrance.com>
2025-01-10 15:13:15 +0100

    feat: Add more tools & config to home-manager, split config into its separate file

---
+++ configuration.nix
@@ -0,0 +1,125 @@
+{ pkgs, lib, inputs, ...}:
+{
+  # Users
+  users.knownUsers = [ "just.maiyak" ];
+  users.users."just.maiyak" = {
+    uid = 501;
+    name = "just.maiyak";
+    home = "/Users/just.maiyak";
+    shell = pkgs.bashInteractive;
+  };
+
+  # Packages
+  environment.systemPackages =
+    with pkgs; [
+      # System Tools
+      bashInteractive
+      bat
+      dust
+      eza
+      fd
+      fzf
+      gnupg
+      neofetch
+      neovim
+      nix-direnv
+      openssl
+      ripgrep
+      skhd
+      starship
+      tldr
+      tokei
+      tre-command
+      vim
+      zoxide
+
+      # Virtualisation
+      colima
+      dive
+      docker
+      docker-compose
+
+      # Languages
+      nixfmt-rfc-style
+
+      # Fonts
+      nerd-fonts.monoid
+      nerd-fonts.jetbrains-mono
+      nerd-fonts.anonymice
+      nerd-fonts.commit-mono
+    ];
+
+  # Homebrew packages
+  homebrew = {
+    enable = true;
+
+    onActivation = {
+      autoUpdate = true;
+      cleanup = "uninstall";
+      upgrade = true;
+    };
+
+    taps = [];
+    brews =
+      [ "git"
+      ];
+    casks = 
+      [ "microsoft-teams"
+        "kitty"
+        "min"
+        "audacity"
+        "zed"
+        "openvpn-connect"
+        "deezer"
+        "bruno"
+        "whatsapp"
+      ];
+    masApps =
+      { Dashlane = 517914548;
+        Powerpoint = 462062816;
+        Excel = 462058435;
+        Word = 462054704;
+        Outlook = 985367838;
+        Slack = 803453959;
+        Messenger = 1480068668;
+      };
+  };
+  environment.variables.HOMEBREW_NO_ANALYTICS = "1"; # No telemetry
+
+  # Necessary for using flakes on this system.
+  nix.settings.experimental-features = "nix-command flakes";
+
+  # Disable nix channel
+  nix.channel.enable = false;
+
+  # Enable alternative shell support in nix-darwin.
+  programs.bash.enable = true;
+
+  # Set Git commit hash for darwin-version.
+  system.configurationRevision = inputs.self.rev or inputs.self.dirtyRev or null;
+
+  # Used for backwards compatibility, please read the changelog before changing.
+  # $ darwin-rebuild changelog
+  system.stateVersion = 5;
+
+  # The platform the configuration will be used on.
+  nixpkgs.hostPlatform = "aarch64-darwin";
+
+  # Use TouchID for sudo login
+  security.pam.enableSudoTouchIdAuth = true;
+
+  # x86_64 support
+  nix.extraOptions = ''
+    extra-platforms = x86_64-darwin aarch64-darwin
+  '';
+
+  # macOS options
+  system.defaults = {
+    dock.autohide = true;
+    dock.mru-spaces = false;
+    finder.AppleShowAllExtensions = true;
+    finder.FXPreferredViewStyle = "clmv";
+    screencapture.location = "~/Pictures/Screenshots";
+    screensaver.askForPasswordDelay = 10;
+  };
+}
--- home.nix
+++ home.nix
@@ -1,6 +1,11 @@
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
+let zoxideHandle = "cd"; in
 {
   home.stateVersion = "24.11";
+
+  home.packages = with pkgs; [
+    nerd-fonts.jetbrains-mono
+  ];
 
   programs = {
 
@@ -8,14 +13,240 @@
       enable = true;
       enableBashIntegration = true;
       nix-direnv.enable = true;
+
+      config.global.hide_env_diff = true;
+      config.global.load_dotenv = true;
     };
 
     bash = {
       enable = true;
       shellAliases = {
-        ls = "ls --color";
-        ll = "ls -al --color";
+        # Handy
+        aepy = "./.venv/bin/activate";
+        cat = "bat";
+        du = "dust";
+        jk = ''${zoxideHandle} "$STRUKTUR_PATH"'';
+        ls = "exa --color";
+        ll = "exa -al --color";
+        la = "exa -al --color";
+        vim = "nvim";
+
+        # Docker
+        d = "docker";
+        dc = "docker container";
+        di = "docker image";
+        dn = "docker network";
+        dv = "docker volume";
+
+        # k8s
+        k = "kubectl";
+        kxt = "kubectl config use-context";
+
+        # ssh kitten
+        ssh = ''if [ $TERM = "xterm-kitty" ]; then TERM="xterm-256color" kitty +kitten ssh; fi'';
       };
+    };
+
+    kitty = {
+      enable = true;
+      shellIntegration.enableBashIntegration = true;
+      themeFile = "Dracula";
+      font = {
+        name = "JetBrainsMono Nerd Font";
+	package = pkgs.nerd-fonts.jetbrains-mono;
+        size = 18;
+      };
+    };
+
+    starship = {
+      enable = true;
+      enableBashIntegration = true;
+      settings = {
+        format = lib.concatStrings [
+          "$username"
+          "$hostname"
+          "$directory"
+          "$git_branch"
+          "$git_state"
+          "$git_status"
+          "$cmd_duration"
+          "$line_break"
+          "$python"
+          "$kubernetes"
+          "$aws$gcloud"
+          "$os"
+          "$character"
+        ];
+
+        command_timeout = 500;
+
+        line_break = {
+          disabled = false;
+        };
+
+        username = {
+          show_always = true;
+          style_user = "bold cyan";
+          format = "[$user]($style) ";
+        };
+
+        directory = {
+          style = "bold bright-blue";
+          read_only = " ";
+          fish_style_pwd_dir_length = 2;
+        };
+
+        character = {
+          success_symbol = "[λ](bold 27)";
+          error_symbol = "[λ](bold red)";
+          vimcmd_symbol = "[❮](bold green)";
+          vimcmd_replace_symbol = "[❮](bold yellow)";
+        };
+
+        kubernetes = {
+          disabled = false;
+          symbol = "⎈";
+          format = "[$symbol $context ~ $namespace ](dimmed blue)";
+        };
+
+        git_branch = {
+          format = "[$branch]($style)";
+          style = "bright-black";
+        };
+
+        git_status = {
+          format = "[[(*$conflicted$untracked$modified$staged$renamed$deleted)](218) ($ahead_behind$stashed)]($style)";
+          style = "cyan";
+          conflicted = "​";
+          untracked = "​";
+          modified = "​";
+          staged = "​";
+          renamed = "​";
+          deleted = "​";
+          stashed = "≡";
+        };
+
+        git_state = {
+          format = "\([$state( $progress_current/$progress_total)]($style)\) ";
+          style = "bright-black";
+        };
+
+        cmd_duration = {
+          format = "[$duration]($style) ";
+          style = "yellow";
+        };
+
+        python = {
+          format = "[$symbol$pyenv_prefix($version )($virtualenv )]($style)";
+          pyenv_version_name = true;
+          detect_extensions = ["py" "ipynb"];
+          style = "green";
+          symbol = " ";
+        };
+
+        aws = {
+          symbol = "a :";
+          style = "dimmed 208"; # dimmed orange
+        };
+
+        gcloud = {
+          symbol = "g ";
+          style = "dimmed cyan";
+          format = "[$symbol:$account(\($region\))]($style) ";
+        };
+
+        conda = {
+          symbol = " ";
+        };
+
+        dart = {
+          symbol = " ";
+        };
+
+        docker_context = {
+          symbol = " ";
+        };
+
+        elixir = {
+          symbol = " ";
+        };
+
+        elm = {
+          symbol = " ";
+        };
+
+        golang = {
+          symbol = " ";
+        };
+
+        hg_branch = {
+          symbol = " ";
+        };
+
+        java = {
+          symbol = " ";
+        };
+
+        julia = {
+          symbol = " ";
+        };
+
+        memory_usage = {
+          symbol = " ";
+        };
+
+        nim = {
+          symbol = " ";
+        };
+
+        nix_shell = {
+          symbol = " ";
+        };
+
+        package = {
+          symbol = " ";
+        };
+
+        perl = {
+          symbol = " ";
+        };
+
+        php = {
+          symbol = " ";
+        };
+
+        ruby = {
+          symbol = " ";
+        };
+
+        rust = {
+          symbol = " ";
+        };
+
+        scala = {
+          symbol = " ";
+        };
+
+        shlvl = {
+          symbol = " ";
+        };
+
+        swift = {
+          symbol = "ﯣ ";
+        };
+
+      };
+    };
+
+    fzf = {
+      enable = true;
+      enableBashIntegration = true;
+    };
+
+    zoxide = {
+      enable = true;
+      enableBashIntegration = true;
+      options = [ "--cmd" zoxideHandle ];
     };
 
     home-manager.enable = true;
