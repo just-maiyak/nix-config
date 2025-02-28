98f3460b76c66efff69ad04962b0dec056200470
Marc Yefimchuk <marc.yefimchuk@radiofrance.com>
2025-02-28 12:02:08 +0100

    feat: Use Lix

--- flake.nix
+++ flake.nix
@@ -7,15 +7,19 @@
     nix-darwin.url = "github:LnL7/nix-darwin";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
 
+    lix-module.url = "https://git.lix.systems/lix-project/nixos-module/archive/2.92.0.tar.gz";
+    lix-module.inputs.nixpkgs.follows = "nixpkgs";
+
     home-manager.url = "github:nix-community/home-manager/master";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = inputs@{ self, nix-darwin, nixpkgs, home-manager }:
+  outputs = inputs@{ self, nix-darwin, nixpkgs, lix-module, home-manager }:
   {
     darwinConfigurations."stallion" = nix-darwin.lib.darwinSystem {
       modules =
         [ ./configuration.nix
+	  lix-module.nixosModules.default
           inputs.home-manager.darwinModules.home-manager
           {
             home-manager.useGlobalPkgs = true;
